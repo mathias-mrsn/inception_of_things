a6a459bc1ddf5ee80b567dc393673268ef2fd860
mathias-mrsn <mathias.mrsn@gmail.com>
2023-09-26 13:52:37 +0200

    p2 : done (Apple Silicon)

--- p2/scripts/k3s.apply.master.sh
+++ p2/scripts/k3s.apply.master.sh
@@ -16,54 +16,45 @@ run() {
     fi
 }
 
-whoami
-
 run \
     "/usr/local/bin/k3s kubectl apply -f /vagrant/confs/configmap.all.yaml" \
     "Creating ConfigMap for every app..." \
     "ConfigMaps created."
 
-while [ $(/usr/local/bin/k3s kubectl get configmap | grep "configmap-app" | wc -l ) != 3 ]
-do
-   echo "[$(date +'%m_%d__%H:%M:%S')] INFO    : Waiting ConfigMap to be accessible"
-   sleep 2;
-done
-
 for f in /vagrant/confs/deployment.app* ; do
 
     pod_name=$(echo "$f" | cut -d"." -f2)
-
-    echo $pod_name
 
     run \
         "/usr/local/bin/k3s kubectl apply -f $f" \
         "Deploying pod \"$pod_name\"..." \
         "Pod \"$pod_name\" deployed."
 
-    while [ $(/usr/local/bin/k3s kubectl get pods --all-namespaces --selector app=$pod_name | grep "Running" | wc -l) -ge 1 ]
+    while [ $(/usr/local/bin/k3s kubectl get pods 2>/dev/null | grep "$pod_name" | grep "Running" | wc -l) == 0 ]
     do
-        echo "[$(date +'%m_%d__%H:%M:%S')] INFO    : Waiting pods \"$pod_name\" to be ready"
-        sleep 2;
+        echo "[$(date +'%m_%d__%H:%M:%S')] INFO    : Waiting pods \"$pod_name\" to be ready."
+        sleep 5;
     done
-    /usr/local/bin/k3s kubectl get pods --all-namespaces --selector app=$pod_name | grep "Running" | wc -l
+    
+    echo "[$(date +'%m_%d__%H:%M:%S')] SUCCESS : Pod \"$pod_name\" is running."
 done
 
 for f in /vagrant/confs/service.app* ; do
 
     service_app_name=$(echo "$f" | cut -d"." -f2)
 
-    echo $service_app_name
-
     run \
         "/usr/local/bin/k3s kubectl apply -f $f" \
         "Deploying service \"$service_app_name\"..." \
-        "Service \"$pod_name\" deployed."
+        "Service \"$service_app_name\" deployed."
 
     while [ $(/usr/local/bin/k3s kubectl get service | grep "$service_app_name" | wc -l) != 1 ]
     do
-        echo "[$(date +'%m_%d__%H:%M:%S')] INFO    : Waiting service \"$pod_name\" to be ready"
-        sleep 2;
+        echo "[$(date +'%m_%d__%H:%M:%S')] INFO    : Waiting service \"$service_app_name\" to be ready"
+        sleep 5;
     done
+
+    echo "[$(date +'%m_%d__%H:%M:%S')] SUCCESS : Service \"$service_app_name\" is running."
 done
 
 run \
@@ -74,7 +65,7 @@ run \
 while [ $(/usr/local/bin/k3s kubectl get ingress | grep "ingress-all" | wc -l ) != 1 ]
 do
    echo "[$(date +'%m_%d__%H:%M:%S')] INFO    : Waiting Ingress to be accessible"
-   sleep 2;
+   sleep 5;
 done
 
 
