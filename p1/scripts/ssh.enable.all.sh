b99931eb1c9d2a92070b0543aa5facebba5bfc05
mathias-mrsn <mathias.mrsn@gmail.com>
2023-10-10 23:19:10 +0200

    ssh password removed

--- p1/scripts/ssh.enable.all.sh
+++ p1/scripts/ssh.enable.all.sh
@@ -17,9 +17,19 @@ run() {
 }
 
 run \
-    "sed -i 's/^PasswordAuthentication no$/PasswordAuthentication yes/' /etc/ssh/sshd_config" \
+    "sed -i 's/^#PasswordAuthentication yes$/PasswordAuthentication yes/' /etc/ssh/sshd_config" \
     "Changing sshd_config..." \
     "sshd_config has been updated."
+
+run \
+    "sed -i 's/^#PermitEmptyPasswords no$/PermitEmptyPasswords yes/' /etc/ssh/sshd_config" \
+    "Changing sshd_config..." \
+    "sshd_config has been updated."
+
+run \
+    "passwd --delete vagrant" \
+    "Removing vagrant user password..." \
+    "vagrant password removed."
 
 run \
     "systemctl restart sshd" \
--- p2/scripts/ssh.enable.sh
+++ p2/scripts/ssh.enable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILENAME="ssh.enable"
+FILENAME="ssh.enable.all"
 LOGFILE="/vagrant/.log/$FILENAME.$(hostname).log"
 
 printf "$FILENAME\n\n"
@@ -17,9 +17,19 @@ run() {
 }
 
 run \
-    "sed -i 's/^PasswordAuthentication no$/PasswordAuthentication yes/' /etc/ssh/sshd_config" \
+    "sed -i 's/^#PasswordAuthentication yes$/PasswordAuthentication yes/' /etc/ssh/sshd_config" \
     "Changing sshd_config..." \
     "sshd_config has been updated."
+
+run \
+    "sed -i 's/^#PermitEmptyPasswords no$/PermitEmptyPasswords yes/' /etc/ssh/sshd_config" \
+    "Changing sshd_config..." \
+    "sshd_config has been updated."
+
+run \
+    "passwd --delete vagrant" \
+    "Removing vagrant user password..." \
+    "vagrant password removed."
 
 run \
     "systemctl restart sshd" \
