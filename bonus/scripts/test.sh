4acf5a62794e1eccfd28924191e892dcb4d11737
Xavier Challe <xchalle@student.42.fr>
2023-10-11 22:42:03 +0200

    done

--- bonus/scripts/test.sh
+++ bonus/scripts/test.sh
@@ -1,10 +1,21 @@
 ORIGINAL_DIR=$(pwd)
-
+GITLAB_TOKEN=$(cat /etc/gitlab/.gitlab_access_token)
 RAND=$(date +'%m_%d__%H:%M:%S')
+
 RED='\e[31m'    # Red text
 GREEN='\e[32m'  # Green text
 BLUE='\e[34m'   # Blue text
 RESET='\e[0m'   # Reset text color to default
+
+if [ $# -ne 1 ]; then
+    echo "$0: usage: ./$0 <HOST_IP>"
+    exit 1
+fi
+
+sed -i "/^    repoURL:/c\    repoURL: 'http://$1:80/root/xchalle_appconf.git'" ../confs/application/application.yaml 
+
+#Create a public repository
+curl --request POST --header "PRIVATE-TOKEN: ${GITLAB_TOKEN}" --data "name=xchalle_appconf&visibility=public" "http://127.0.0.1:80/api/v4/projects"
 
 #Clone the app repo in GitLab
 sudo apt-get install git
@@ -13,7 +24,7 @@ cd tmp_${RAND} && \
 git checkout bonus && \
 rm -rf .git && \
 git init && \
-git remote add origin http://root:ThisIsMyPassword42@127.0.0.1/root/xchalle_appconf.git && \
+git remote add origin http://root:${GITLAB_TOKEN}@127.0.0.1/root/xchalle_appconf.git && \
 git add . && \
 git config --global user.email "xchalle@student.42.fr" && \
 git config --global user.name "Xavier Challe" && \
@@ -24,7 +35,7 @@ rm -rf ~/tmp_${RAND} && \
 
 #Apply the ArgoCD application
 sudo kubectl config use-context k3d-my-cluster
-sudo kubectl apply -f ./confs/application/application.yaml
+sudo kubectl apply -f ../confs/application/application.yaml
 sudo kubectl config use-context k3d-gitlab
 
 echo -e "${BLUE}Bonus application -> http://localhost:8888/bonus"
